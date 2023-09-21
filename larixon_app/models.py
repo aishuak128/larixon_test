ae5a680f8f496a8371cfe05f929f644897add46c
Aishuak <aishuak99@mail.ru>
2023-09-22 03:14:55 +0600

    did some cleanup

--- larixon_app/models.py
+++ larixon_app/models.py
@@ -7,7 +7,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=100, blank=True, default='')
-    print('ffffdsfkdlsjaflkdjsaflkdjsafljdlskaf;dlsakjfjdklsajfdklasjflkdasjflkdsajflkdsajfkldsafdsfdsfdsfdsfdsfdsfdsfdsfdsfds')
+    
     def __str__(self):
        return self.name
 
